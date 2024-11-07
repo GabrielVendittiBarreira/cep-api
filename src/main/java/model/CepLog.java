b3c828388a956b2acc3df69816f8c7639f11d18c
Gabriel Venditti Barreira <111370987+GabrielVendittiBarreira@users.noreply.github.com>
2024-11-07 05:50:36 +0000

    update test

--- src/main/java/model/CepLog.java
+++ src/main/java/model/CepLog.java
@@ -14,4 +14,35 @@ public class CepLog {
     private LocalDateTime timestamp;
 
     // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getResponse() {
+        return response;
+    }
+
+    public void setResponse(String response) {
+        this.response = response;
+    }
+
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(LocalDateTime timestamp) {
+        this.timestamp = timestamp;
+    }
 }
